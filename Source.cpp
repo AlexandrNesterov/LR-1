5b1d63d664f084371dab569abfffc4aad9ad6c5f
AlexandrNesterov <71148837+AlexandrNesterov@users.noreply.github.com>
2021-01-30 16:37:55 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,10 +2,11 @@
 #include <stdio.h>
 int main() {
 	int a[10];
+	int razn[9];
 
 	for (int i = 0; i < 10; i++)
 	{
-		printf("%d: ", i + 1);
+		printf("vvedite chislo %d: ", i + 1);
 		scanf("%d", &a[i]);
 
 	}
@@ -18,7 +19,7 @@ int main() {
 	for (int i = 0; i < 9; i++)
 	{
 		int b = a[i] - a[i + 1];
-		printf("%d: %d - %d = %d\n", i + 1, a[i], a[i + 1], b);
+		printf("raznost %d: %d - %d = %d\n", i + 1, a[i], a[i + 1], b);
 		if (b < 0) {
 			b = b * -1;
 		}
@@ -26,8 +27,26 @@ int main() {
 		if (b < min) {
 			min = b;
 		}
+		razn[i] = b;
 	}
+	for (int i = 1; i < 9; i++) {
+		int k = i;
+		while (razn[k - 1] > razn[k] && k > 0) {
+			int tmp = razn[k - 1];
+			razn[k - 1] = razn[k];
+			razn[k] = tmp;
+			k = k - 1;
+		}
+
+	}
+	int mediana = razn[4];
+
+	for (int i = 0; i < 9; i++) {
+		
+		printf("%d: %d\n",i+1, razn[i]);
+	}
+	
+	printf("mediana: %d\n", mediana);
 
 	printf("min razn %d", min);
 	return 0;
-}
